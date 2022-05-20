feb513e7f3883c730a75f008d94dd340d63e77c8
DmitShu <dmitshu@gmail.com>
2022-05-20 09:31:40 +0300

    fixture

--- Fixtures.py
+++ Fixtures.py
@@ -2,6 +2,34 @@ import pytest
 import requests
 from datetime import datetime
 
+email = "Test19856@Test19856"
+password = "Test19856"
+
+# function — запускается для каждого теста по умолчанию любая фикстура имеет scope=«function»;
+# class — запускается для каждого тестового класса;
+# module — запускается для каждого модуля;
+# package — запускается для каждого пакета;
+# session — запускается один раз перед всеми тестами.
+
+
+# @pytest.fixture(scope="class", autouse=True)
+# def session_fixture():
+#     print("\nclass fixture starts")
+#
+# @pytest.fixture(scope="function", autouse=True)
+# def function_fixture():
+#     print("\nfunction fixture starts")
+#
+#
+# class TestClass23:
+#
+#     def test_first(self):
+#         pass
+#
+#     def test_second(self):
+#         pass
+
+
 @pytest.fixture(autouse=True)
 def time_delta():
     start_time = datetime.now()
@@ -15,9 +43,6 @@ def some_data():
 
 def test_some_data(some_data):
     assert some_data == 42
-
-email = "Test19856@Test19856"
-password = "Test19856"
 
 @pytest.fixture()
 def get_key():
