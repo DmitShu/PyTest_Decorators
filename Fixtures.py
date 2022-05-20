768535136de2b13889afc20f69cf203027acae41
DmitShu <dmitshu@gmail.com>
2022-05-20 09:44:48 +0300

    request

--- Fixtures.py
+++ Fixtures.py
@@ -44,16 +44,50 @@ def some_data():
 def test_some_data(some_data):
     assert some_data == 42
 
-@pytest.fixture()
-def get_key():
+# @pytest.fixture()
+# def get_key():
+#     response = requests.post(url='https://petfriends1.herokuapp.com/login',
+#                              data={"email": email, "pass": password})
+#     assert response.status_code == 200, 'Запрос выполнен неуспешно'
+#     assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
+#     return response.request.headers.get('Cookie')
+#
+# def test_getAllPets(get_key):
+#     response = requests.get(url='https://petfriends1.herokuapp.com/api/pets',
+#                             headers={"Cookie": get_key})
+#     assert response.status_code == 200, 'Запрос выполнен неуспешно'
+#     assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
+
+@pytest.fixture(scope="class")
+def get_key(request):
+
     response = requests.post(url='https://petfriends1.herokuapp.com/login',
                              data={"email": email, "pass": password})
     assert response.status_code == 200, 'Запрос выполнен неуспешно'
     assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
+    print("\nreturn auth_key")
     return response.request.headers.get('Cookie')
 
-def test_getAllPets(get_key):
-    response = requests.get(url='https://petfriends1.herokuapp.com/api/pets',
-                            headers={"Cookie": get_key})
-    assert response.status_code == 200, 'Запрос выполнен неуспешно'
-    assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
+
+@pytest.fixture(autouse=True)
+def request_fixture(request):
+    if 'Pets' in request.function.__name__:
+        print(f"\nЗапущен тест из сьюта Дом Питомца: {request.function.__name__}")
+
+
+class TestClassPets:
+
+    def test_getAllPets2(self, get_key):
+        response = requests.get(url='https://petfriends1.herokuapp.com/api/pets',
+                                headers={"Cookie": get_key})
+        assert response.status_code == 200, 'Запрос выполнен неуспешно'
+        assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
+
+    def test_getMyPets2(self, get_key):
+        response = requests.get(url='https://petfriends1.herokuapp.com/my_pets',
+                                headers={"Cookie": get_key})
+        assert response.status_code == 200, 'Запрос выполнен неуспешно'
+        assert response.headers.get('Content-Type') == 'text/html; charset=utf-8'
+
+    def test_anotherTest(self):
+        pass
